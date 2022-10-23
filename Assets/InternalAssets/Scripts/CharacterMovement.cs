1eae5d7812bad69843d6a1260bf06f87643d69b8
Ayesha Siroy <siroy.ayesha@gmail.com>
2022-10-23 21:32:57 +0800

    feat: add input system for movement

--- Assets/InternalAssets/Scripts/CharacterMovement.cs
+++ Assets/InternalAssets/Scripts/CharacterMovement.cs
@@ -1,41 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
     CharacterController characterController;
     Animator animator;
-    [SerializeField] float moveSpeed = 0.5f;
-    [SerializeField] float rotationSpeed = 1f;
-    [SerializeField] float motionSmoothTime = 0.1f;
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float motionSmoothTime = 0.25f;
+    PlayerControls playerControls;
 
-    Vector3 movement;
+    public Vector3 move;
+    float currentSpeed;
+    float speedVelocity;
     float verticalSpeed = 0;
     float gravity = 9.87f;
-    float verticalMouseInput;
+    private State state;
 
+    private enum State{
+        Idle,
+        TurnToMove
+    }
+
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+        playerControls.Player.Player1Move.started += ctx => OnMove(ctx);
+        // playerControls.Player.Player1Move.canceled += ctx => OnMove(ctx);
     }
+
+    void OnMove(InputAction.CallbackContext context)
+    {
+        Move();
+    }
+
+    // void OnStop(InputAction.CallbackContext context)
+    // {
+    //     StopCoroutine(Movement());
+    // }
 
     private void Update()
     {
         Move();
     }
-
-    private void Move()
+    void Move()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 characterControllerCurrentPos = characterController.transform.position;
-        movement = new Vector3(horizontalInput, 0f, verticalInput);
+        Vector2 movement = playerControls.Player.Player1Move.ReadValue<Vector2>();
+        move = new Vector3(movement.x, 0, movement.y);
+        move = (transform.forward * movement.y) + (transform.right * movement.x);
+        move = move.normalized;
 
-        movement = movement.normalized * moveSpeed;
-        movement = transform.worldToLocalMatrix.inverse * movement;
 
         if (characterController.isGrounded)
         {
@@ -43,23 +76,32 @@ public class CharacterMovement : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                movement.y = 8f;
+                move.y = 8f;
             }
 
         }
 
-        // apply gravity acceleration to vertical speed
-        verticalSpeed -= gravity;
-        movement.y = verticalSpeed;
+        //char rotation
+        characterController.transform.Rotate(Vector3.up * movement.x * (100f * Time.deltaTime));
 
-        characterController.Move(movement);
+        // if (move != Vector3.zero)
+        // {
+        //     float rotation = Mathf.Atan2(move.x, move.y) * Mathf.Rad2Deg + playerCam.eulerAngles.y;
+        //     transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref currentVelocity, rotationSmoothTime);
+        // }
+
+        float targetSpeed = moveSpeed * move.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedVelocity, 0.1f);
+
+        verticalSpeed -= gravity;
+        move.y = verticalSpeed;
+
+        characterController.Move(move * currentSpeed * Time.deltaTime);
 
         Vector3 horizontalVelocity = characterController.velocity;
         horizontalVelocity = new Vector3(characterController.velocity.x, 0, characterController.velocity.z);
         float horizontalSpeed = horizontalVelocity.magnitude;
 
-        //(string name, float value, float dampTime, deltaTime)
         animator.SetFloat("Speed", horizontalSpeed, motionSmoothTime, Time.deltaTime);
     }
-
 }
