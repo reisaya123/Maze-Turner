da76543d901a31865549cfa6bbc191e1b46eb451
Ayesha Siroy <siroy.ayesha@gmail.com>
2022-10-23 21:34:33 +0800

    feat: improve camera movement

--- Assets/InternalAssets/Scripts/Camera/CameraMovement.cs
+++ Assets/InternalAssets/Scripts/Camera/CameraMovement.cs
@@ -4,20 +4,38 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] float mouseSensitivity = 1f;
-    Transform player;
-    float verticalInput;
+    [SerializeField] float mouseSensitivity = 10f;
+    // Transform player;
 
-    void Start() {
-        player = transform.root.GetComponent<Transform>();
-    }
+    Vector3 targetRotation;
+    float verticalInput;
+    float horizontalInput;
+
+    float rotationMin = -5f;
+    float rotationMax = 80f;
+    [SerializeField] float smoothTime = 0.12f;
+    Vector3 currentVel;
+
+    public Transform target;
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        verticalInput += Input.GetAxis("Mouse X") * mouseSensitivity;
 
-        transform.rotation = Quaternion.Euler(0f, verticalInput, 0f);
-        player.transform.rotation = Quaternion.Euler(0f, verticalInput, 0f);
+        verticalInput += Input.GetAxis("Mouse X") * mouseSensitivity;
+        horizontalInput -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        horizontalInput = Mathf.Clamp(horizontalInput, rotationMin, rotationMax);
+
+        targetRotation = Vector3.SmoothDamp(targetRotation, new Vector3(horizontalInput, verticalInput), ref currentVel, smoothTime);
+        transform.eulerAngles = targetRotation;
+        // target.eulerAngles = targetRotation;
+        // Quaternion newRotation = Quaternion.Euler(0f,verticalInput,0f);
+        transform.position = target.position - transform.forward * 5f;
+
+
+        // transform.rotation = Quaternion.Euler(0f, verticalInput, 0f);
+        // transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime);
+        // player.transform.rotation = Quaternion.Euler(0f, verticalInput, 0f);
     }
 }
