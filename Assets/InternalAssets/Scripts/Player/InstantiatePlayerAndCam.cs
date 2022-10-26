7b89614cef5db49bbc3b5036af8684dd613e8978
Ayesha Siroy <siroy.ayesha@gmail.com>
2022-10-27 00:35:37 +0800

    feat: instantiate player with cam

--- Assets/InternalAssets/Scripts/Player/InstantiatePlayerAndCam.cs
+++ Assets/InternalAssets/Scripts/Player/InstantiatePlayerAndCam.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InstantiatePlayers : MonoBehaviour
+public class InstantiatePlayerAndCam : MonoBehaviour
 {
     public List<GameObject> animalModels;
+    public GameObject mainCam;
     // Start is called before the first frame update
     void Start()
     {
-        GameManager.instance.InstantiatePlayerAnimals(animalModels);
+        GameManager.instance.InstantiatePlayerAndCam(animalModels,mainCam);
     }
 
 }
