3ff0e2f0fee6bad64610750a6e0e27b2f327f6cc
elim <elimjoyce.abagat-19@gmail.com>
2022-11-03 02:52:57 +0800

    feat: add spawnchest script

--- Assets/InternalAssets/Scripts/SpawnChest.cs
+++ Assets/InternalAssets/Scripts/SpawnChest.cs
@@ -5,38 +5,39 @@ using UnityEngine;
 public class SpawnChest : MonoBehaviour
 {
     public List<GameObject> chestPositions = new List<GameObject>();
-    [SerializeField] Transform chestModel;
+    public List<GameObject> spawnChests = new List<GameObject>();
+    [SerializeField] GameObject chestModel;
     int randomizeSpawning;
     int randomizeChestType;
     int count = 0;
 
     void Start() {
 
-    // foreach (Transform chest in transform)
-    // {
-    //     chestPositions.Add(chest.gameObject);
-    // }
+    foreach (Transform chest in transform)
+    {
+        chestPositions.Add(chest.gameObject);
+    }
 
-    // int currentRandomizeSpawning = randomizeSpawning
-    // while (true)
-    // {
-    //     if(count == 3){
-    //         return;
-    //     }
-    //             randomizeSpawning = Random.Range(0, 13);
-    //     if(currentRandomizeSpawning != randomizeSpawning){
-    //     GameObject chest =    Instantiate(chestModel)
-    //     Renderer positionBoxRenderer = chestPositions[randomizeSpawning].GetComponent<Renderer>();
-    // chest.position = positionBoxRenderer.bounds.center; 
-    // count++;
-    //     }
-    // }
-    // for(count=0; count < 3; count++){[
+    while (true)
+    {
 
+        if(count == 3){
+            return;
+        }
+        randomizeSpawning = Random.Range(0, chestPositions.Count-1);
+        Debug.Log(randomizeSpawning);
+        Debug.Log(chestPositions.Count);
 
-    // ]}
-
+        if (!spawnChests.Contains(chestPositions[randomizeSpawning]) )
+        {
+            spawnChests.Add(chestPositions[randomizeSpawning]);
+            Renderer positionBoxRenderer = chestPositions[randomizeSpawning].GetComponent<Renderer>();
             
+            GameObject chest = Instantiate(chestModel, positionBoxRenderer.bounds.center, Quaternion.identity);
+            count++;
+        }
+        // Destroy(transform.Find(chestPositions[randomizeSpawning].name).gameObject);
+    }
     }
 
 }
